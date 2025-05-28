f2d9317ac43152bed499dd2712a2f99644aa4789
javiern-778 <jcruzespana2@gmail.com>
2025-05-27 23:21:53 -0600

    Agregar registro de usuarios

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,4 @@
 // src/api.js
-export const API_URL = 'https://app-1-6nr0.onrender.com';
+// export const API_URL = 'https://app-1-6nr0.onrender.com';
+
+export const API_URL = import.meta.env.VITE_API_URL;
