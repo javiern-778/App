8dd1e3d8b8454cbaeadb84121118d85b47d847a0
javiern-778 <jcruzespana2@gmail.com>
2025-05-27 23:45:36 -0600

    c

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,5 @@
 // src/api.js
-export const API_URL = 'https://app-1-6nr0.onrender.com';
+export const API_URL = 'https://app-1-6nr0.onrender.com/api';
+
 
 // export const API_URL = import.meta.env.VITE_API_URL;
